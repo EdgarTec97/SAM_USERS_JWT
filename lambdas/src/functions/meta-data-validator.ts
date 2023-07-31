77d07ad7d0423580e4e02477465d596fe63e47e3
Edgar Castillo Vega <edgar.castillo@skydropx.com>
2023-07-31 09:27:24 -0600

    feat: the s3 feature and destination feature were implemented with all necessary workflows in the template yaml (SAM) [v1]

--- lambdas/src/functions/meta-data-validator.ts
+++ lambdas/src/functions/meta-data-validator.ts
@@ -1,6 +1,6 @@
 import { Handler, S3Event } from 'aws-lambda';
 
-const MetaDataValidator = async (event: S3Event): Promise<void> => {
+const MetaDataValidator = async (event: S3Event): Promise<Object> => {
   try {
     for (const record of event.Records) {
       console.info(JSON.stringify(record));
@@ -9,7 +9,10 @@ const MetaDataValidator = async (event: S3Event): Promise<void> => {
     console.error('Error in the meta-data validator lambda [fn]', error);
   }
 
-  return;
+  if (event.Records[0].s3.object.key.toLowerCase().includes('pantalla'))
+    throw new Error('Invalid format');
+
+  return { message: JSON.stringify(event), data: 'SUCCESS_AWS_ECV' };
 };
 
 export const handler: Handler = MetaDataValidator;
